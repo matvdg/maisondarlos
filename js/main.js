238a1aab2669755ccf720abdc12c222cbed7f74d
Mathieu Vandeginste <matvdg@me.com>
2021-02-23 09:53:02 +0100

    feat(wip): Add wip page

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const $form = document.querySelector('form#mailing-list')
+
+$form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const formData = new FormData($form)
+
+  fetch('/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  }).then(() => {
+    // @todo
+  }).catch((error) => {
+    // @todo
+  })
+    
+})
