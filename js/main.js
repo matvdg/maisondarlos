918ab1947153b2e60c0138f04b52fbf570c1510c
Mathieu Vandeginste <matvdg@me.com>
2021-04-09 17:48:44 +0200

    fix: alert

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 const $form = document.querySelector("form#mailing-list")
 
-$form.addEventListener("submit", (event) => {
-  event.preventDefault()
+$form.addEventListener("submit", () => {
+  // event.preventDefault()
+  alert("Merci de vous être inscrit, on vous tient informé !")
+  //   const formData = new FormData($form)
 
-  const formData = new FormData($form)
-
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => {
-      // @todo
-    })
-    .catch((error) => {
-      // @todo
-    })
+  //   fetch("/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: new URLSearchParams(formData).toString(),
+  //   })
+  //     .then(() => {
+  //       // @todo
+  //     })
+  //     .catch((error) => {
+  //       // @todo
+  //     })
 })
