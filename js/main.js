bcd1d9a36cc868a548ff0d272fc1585f05abfa28
Mathieu Vandeginste <matvdg@me.com>
2021-04-09 16:44:39 +0200

    feat: Our story

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
-const $form = document.querySelector('form#mailing-list')
+const $form = document.querySelector("form#mailing-list")
 
-$form.addEventListener('submit', (event) => {
+$form.addEventListener("submit", (event) => {
   event.preventDefault()
 
   const formData = new FormData($form)
 
-  fetch('/', {
-    method: 'POST',
+  fetch("/", {
+    method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-  }).then(() => {
-    // @todo
-  }).catch((error) => {
-    // @todo
+    body: new URLSearchParams(formData).toString(),
   })
-    
+    .then(() => {
+      // @todo
+    })
+    .catch((error) => {
+      // @todo
+    })
 })
--- js/maps.js
+++ js/maps.js
@@ -1,28 +1,28 @@
 var MarkerAnnotation = mapkit.MarkerAnnotation,
-  clickAnnotation;
-var home = new mapkit.Coordinate(42.893374, 0.69989);
+  clickAnnotation
+var home = new mapkit.Coordinate(42.893374, 0.69989)
 
 mapkit.init({
   authorizationCallback: function (done) {
     done(
       "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkxZRlM2Q1gzOTMifQ.eyJpc3MiOiIzV0FRRjNBUzgyIiwiaWF0IjoxNjE0MDkzMDc1LCJleHAiOjE4OTUzMjUwNzUsIm9yaWdpbiI6Imh0dHBzOi8vbWFpc29uZGFybG9zLmZyIn0.Z_-KFV8cGAVNYJlE5JBkLcG7rxYAsgGTugKZ1iG_AF0yCuFUxbi7Ccwenm0XNQHSGilq-JVs06bL7J4hn2LJsg"
-    );
+    )
   },
-});
-var map = new mapkit.Map("map");
+})
+var map = new mapkit.Map("map")
 
 // Setting properties on creation:
 var homeAnnotation = new MarkerAnnotation(home, {
   color: "#26406D",
   title: "Chez Anna & Mathieu",
   glyphImage: { 1: "assets/logo.png" },
-});
+})
 
 // Add and show both annotations on the map
-map.showItems([homeAnnotation]);
+map.showItems([homeAnnotation])
 
 // Configure mapView
 //map.mapType = mapkit.Map.MapTypes.Hybrid
 var span = new mapkit.CoordinateSpan(0.16, 0.16),
-  region = new mapkit.CoordinateRegion(home, span);
-map.region = region;
+  region = new mapkit.CoordinateRegion(home, span)
+map.region = region
